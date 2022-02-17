8ee6fdbbccacea538f38a948faf1b55cf72f502a
Joseph Diza <josephm.diza@gmail.com>
2022-02-17 15:37:56 -0800

    List all pdf files under dir

--- treeleaves.lisp
+++ treeleaves.lisp
@@ -1,11 +1,16 @@
 #!/usr/bin/sbcl --script
 
 (require "uiop")
+(defvar dir "~/Documents")
 
-(defparameter *dirs* nil "All recursive directories.")
+;(defparameter *dirs* nil "All recursive directories.")
 
-(uiop:collect-sub*directories "~/Documents"
-    (constantly t)
-    (constantly t)
-    (lambda (it) (push it *dirs*)))
-(print *dirs*)
+;(uiop:collect-sub*directories dir
+    ;(constantly t)
+    ;(constantly t)
+    ;(lambda (it) (push it *dirs*)))
+;(print *dirs*)
+
+; Expands the directory path, and collects all the pdf files
+(defparameter pdf_files (concatenate 'string (uiop:native-namestring dir) "/**/*.pdf"))
+(directory pdf_files)
