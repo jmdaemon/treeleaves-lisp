00eacbbac73d7837bf47a1c4eb0252e81c3548e7
Joseph Diza <josephm.diza@gmail.com>
2022-02-17 16:22:29 -0800

    Split list of directories on '/' character

--- treeleaves.lisp
+++ treeleaves.lisp
@@ -11,5 +11,9 @@
 (defparameter pdf-dir (concatenate 'string (uiop:native-namestring dir) "/**/*.pdf"))
 (defparameter pdf-files (directory pdf-dir))
 
-; Split on '/' characters
-(cl-utilities:split-sequence #\/ dir)
+(defun split-dir (dirname)
+  "Splits a directory file path on '/' characters"
+    (cl-utilities:split-sequence #\/ dirname))
+
+(loop for filepath in pdf-files
+  do (print (split-dir (uiop:native-namestring filepath))))
