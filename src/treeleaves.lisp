7b18f4787f5bf1f1f479f43938734ec1e5484771
Joseph Diza <josephm.diza@gmail.com>
2022-03-05 17:06:01 -0800

    Add another feature for TODO

--- src/treeleaves.lisp
+++ src/treeleaves.lisp
@@ -25,6 +25,7 @@
 ; - Add table cli argument
 ;   - Replace hardcoded 'document calls with table arg
 ; - Add subcommand feature to find dead filepaths and remove them from the database (reindex)
+; - Add option to only generate and print file tags, file path for a given file
 
 (defun main ()
   "Main application entry point"
